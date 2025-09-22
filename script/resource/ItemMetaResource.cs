9dba6afff0ee9a95c06476bdf825af7d4f656d1f
Dpoxiao <1191531465@qq.com>
2025-09-22 22:40:38 +0800

    更新

--- script/resource/ItemMetaResource.cs
+++ script/resource/ItemMetaResource.cs
@@ -4,11 +4,12 @@ using Godot.Collections;
 namespace horizoncraft.script.resource;
 
 [GlobalClass]
-public partial class ItemMetaResource:Resource
+public partial class ItemMetaResource : Resource
 {
-    public string ItemName;
-    public int MaxAmount;
-    public string Description;
-    public Dictionary<string, string> Tags;
-    public Array<string> Textures;
+    [ExportGroup("基础属性")] [Export] public string ItemName;
+    [ExportGroup("基础属性")] [Export] public int MaxAmount;
+    [ExportGroup("基础属性")] [Export] public string Description;
+    [ExportGroup("高级属性")] [Export] public Array<ComponentsResource> Components;
+    [ExportGroup("高级属性")] [Export] public Dictionary<string, string> Tags;
+    [ExportGroup("高级属性")] [Export] public Array<string> Textures;
 }
